80742da94dfed8e4e1c1701f39758f47794f005d
Yusuf Olaniyi <olaniyiyusuf2000@gmail.com>
2024-02-01 20:29:58 +0100

    Create Arithmetic Operators.py

---
+++ Arithmetic Operators.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    #print the sum
+    print(a+b)
+    
+    #print the difference between a and b
+    print(a-b)
+    
+    # print their product
+    print(a*b)
