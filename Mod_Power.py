3f690e11164e50f5a9773ea4e8f3825ea534c6c8
Yusuf Olaniyi <olaniyiyusuf2000@gmail.com>
2024-02-01 21:07:07 +0100

    Create Mod_Power.py

---
+++ Mod_Power.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+m = int(input())
+#print a raised to power b
+print(a ** b)
+
+#print the modulus of a raised to power b with m
+print((a ** b) % m)
