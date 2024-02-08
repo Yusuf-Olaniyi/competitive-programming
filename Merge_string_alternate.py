6e939b5f12011ce9e46f63c1fb3221ff08f31ce8
Yusuf Olaniyi <olaniyiyusuf2000@gmail.com>
2024-02-08 07:36:36 +0100

    Create Merge_string_alternate.py

---
+++ Merge_string_alternate.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        merged = []
+        l1, l2 = 0, 0
+
+        while l1 < len(word1) and l2 < len(word2):
+            merged.append(word1[l1])
+            merged.append(word2[l2])
+            l1 += 1
+            l2 += 1
+
+        while l1 < len(word1):
+            merged.append(word1[l1])
+            l1 += 1
+
+        while l2 < len(word2):
+            merged.append(word2[l2])
+            l2 += 1
+
+        return ''.join(merged)
+        
